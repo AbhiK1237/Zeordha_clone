6bf5c0f8e985fa49f998f124a8118586230287f6
AbhiK1237 <kaleabhijith@gmail.com>
2025-02-06 15:22:54 +0530

    changes to cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 const corsOptions = {
-    origin:['http://localhost:3000',"http://localhost:3001","https://kitedashboard.vercel.app/"],
+    origin:['http://localhost:3000',"http://localhost:3001","https://kitedashboard.vercel.app"],
     credentials: true,
 } ;
 app.options("*", cors(corsOptions)); // Handle preflight requests globally
